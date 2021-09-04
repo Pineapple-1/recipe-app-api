43ec9fbf1f6acdba3ef8447cf795acc7b0995545
abdulrehman ajmal <abdulrehman.ajmal@outlook.com>
2021-09-04 20:36:19 +0500

    added tags

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -50,9 +50,21 @@ class PrivateTagsApiTest(TestCase):
         Tag.objects.create(user=imposter, name='BAD_FOOD')
         tag = Tag.objects.create(user=self.user, name='fruit')
         res = self.client.get(TAG_URL)
-        self.assertEqual(res.status_code,status.HTTP_200_OK)
-        self.assertEqual(len(res.data),1)
-        self.assertEqual(res.data[0]['name'],tag.name)
-    
-    
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tags_successfull(self):
+        """Test creating new Tag"""
+        payload = {'name': 'Test Tag'}
+        self.client.post(TAG_URL, payload)
+        exists = Tag.objects.filter(user=self.user, name = payload['name']).exists()
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating new tag invalid"""
+        payload = {'name':''}
+        res = self.client.post(TAG_URL,payload)
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+
 
