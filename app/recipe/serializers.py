eded96647f79f97da18008462fcbf6df9ab46cd3
abdulrehman ajmal <abdulrehman.ajmal@outlook.com>
2021-09-06 17:25:17 +0500

    added Ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from django.db.models import fields
 from rest_framework import serializers
-from core.models import Tag
+from core.models import Tag,Ingredient
 
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for tags"""
@@ -9,3 +9,12 @@ class TagSerializer(serializers.ModelSerializer):
         model= Tag
         fields=['id','name']
         read_only_fields = ['id']
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for Ingredient objects"""
+    
+    class Meta:
+        model= Ingredient
+        fields=['id','name']
+        read_only_fields = ['id']
+        
